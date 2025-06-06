f95569b21c6c27b6f4a2c5f86bf82a34c6e8b0bd
Pricilla Dwi Permata <pricilladwi24@gmail.com>
2025-06-06 16:35:11 +0700

    Update webpack.config.js

--- HexaMood/webpack.config.js
+++ HexaMood/webpack.config.js
@@ -1,63 +1,98 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  entry: {
-    main: './src/js/main.js',
-    login: './src/js/login.js',
-    register: './src/js/register.js',
-    checkin: './src/js/checkin.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].bundle.js',
-    clean: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[name][ext]'
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      chunks: ['main']
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/login.html',
-      filename: 'login.html',
-      chunks: ['login']
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/register.html',
-      filename: 'register.html',
-      chunks: ['register']
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/checkin.html',
-      filename: 'checkin.html',
-      chunks: ['checkin']
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles/style.css'
-    })
-  ],
-  devServer: {
-    static: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 8080
-  },
-  mode: 'development'
+module.exports = (env) => {
+  const isDev = env === "development";
+
+  return {
+    mode: isDev ? "development" : "production",
+    entry: {
+      main: "./src/js/main.js",
+      login: "./src/js/login.js",
+      register: "./src/js/register.js",
+      checkin: "./src/js/checkin.js",
+      journaling: "./src/js/journaling.js",
+      results: "./src/js/results.js",
+      recommendations: "./src/js/recommendations.js",
+      navbar: "./src/js/navbar.js",
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "js/[name].bundle.js",
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                url: true,
+                importLoaders: 1,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "images/[name][ext][query]",
+          },
+        },
+      ],
+    },
+    plugins: [
+      ...[
+        "index",
+        "login",
+        "register",
+        "checkin",
+        "journaling",
+        "results",
+        "recommendations",
+      ].map(
+        (name) =>
+          new HtmlWebpackPlugin({
+            template: `./src/${name}.html`,
+            filename: `${name}.html`,
+            chunks: [name === "index" ? "main" : name, "navbar"],
+          })
+      ),
+      ...(isDev
+        ? []
+        : [
+            new MiniCssExtractPlugin({
+              filename: "styles/[name].css",
+            }),
+          ]),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "src/images",
+            to: "images",
+            globOptions: {
+              ignore: ["**/.*"],
+            },
+          },
+          {
+            from: "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+            to: "js/lib/",
+          },
+        ],
+      }),
+    ],
+    devServer: {
+      static: path.join(__dirname, "dist"),
+      compress: true,
+      port: 8080,
+      historyApiFallback: true,
+      hot: true,
+    },
+  };
 };
